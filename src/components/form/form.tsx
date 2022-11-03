27282672bda331323bece2f9e133eadf9140d4b8
Recriando Games <gabriel.mattos17@hotmail.com>
2022-11-03 10:17:55 -0300

    Dropdown

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,12 +1,20 @@
 import { Box } from "@mui/material";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 import { useForm } from "react-hook-form";
 import "./form.css";
 
 const Form = () => {
-
-    const {register, handleSubmit, formState: {errors}} = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <Box
@@ -20,11 +28,52 @@ const Form = () => {
       <div>
         <h1 className="title">Register form</h1>
       </div>
-      <form onSubmit={handleSubmit((data) => {console.log(data)})}>
-        <TextField {...register("firstName", {required: 'First name is required.'})} helperText={errors.firstName?.message?.toString()} label="First Name" variant="filled" />
-        <TextField {...register("lastName", {required: 'Last name is required.', minLength: {value: 4, message: "Minimum length is 4"}})} helperText={errors.lastName?.message?.toString()} label="Last Name" variant="filled" />
-        <TextField {...register("email", {required: 'Email is required.'})} helperText={errors.email?.message?.toString()} label="Last Name" variant="filled" />
-        <Button variant="contained" type="submit">Submit</Button>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <div>
+          <TextField
+            {...register("firstName", { required: "First name is required." })}
+            helperText={errors.firstName?.message?.toString()}
+            label="First Name"
+            variant="filled"
+          />
+          <TextField
+            {...register("lastName", {
+              required: "Last name is required.",
+              minLength: { value: 4, message: "Minimum length is 4" },
+            })}
+            helperText={errors.lastName?.message?.toString()}
+            label="Last Name"
+            variant="filled"
+          />
+        </div>
+        <div>
+          <TextField
+            {...register("email", { required: "Email is required." })}
+            helperText={errors.email?.message?.toString()}
+            label="Last Name"
+            variant="filled"
+          />
+        </div>
+        <div>
+          <FormControl sx={{ m: 0, minWidth: 120 }}>
+            <InputLabel id="country">Country</InputLabel>
+            <Select labelId="country" label="country" {...register("country", {required: "Country is required."})}>
+              <MenuItem value="united states">United States</MenuItem>
+              <MenuItem value="brazil">Brazil</MenuItem>
+              <MenuItem value="canada">Canada</MenuItem>
+            </Select>
+            <FormHelperText>{errors.country?.message?.toString()}</FormHelperText>
+          </FormControl>
+        </div>
+        <div>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </div>
       </form>
     </Box>
   );
