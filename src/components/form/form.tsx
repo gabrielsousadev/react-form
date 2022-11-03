bd43ff97d5df5c3dc6d9a093c6dae895a5b9d595
Recriando Games <gabriel.mattos17@hotmail.com>
2022-11-03 10:01:08 -0300

    Email field

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -6,9 +6,7 @@ import "./form.css";
 
 const Form = () => {
 
-    const {register, handleSubmit, formState: {errors} } = useForm()
-
-    console.log(errors)
+    const {register, handleSubmit, formState: {errors}} = useForm()
 
   return (
     <Box
@@ -23,8 +21,9 @@ const Form = () => {
         <h1 className="title">Register form</h1>
       </div>
       <form onSubmit={handleSubmit((data) => {console.log(data)})}>
-        <TextField {...register("firstName", {required: 'This is required.'})} label="First Name" variant="filled" />
-        <TextField {...register("lastName", {required: 'This is required.', minLength: {value: 4, message: "Minimum length is 4"}})} label="Last Name" variant="filled" />
+        <TextField {...register("firstName", {required: 'First name is required.'})} helperText={errors.firstName?.message?.toString()} label="First Name" variant="filled" />
+        <TextField {...register("lastName", {required: 'Last name is required.', minLength: {value: 4, message: "Minimum length is 4"}})} helperText={errors.lastName?.message?.toString()} label="Last Name" variant="filled" />
+        <TextField {...register("email", {required: 'Email is required.'})} helperText={errors.email?.message?.toString()} label="Last Name" variant="filled" />
         <Button variant="contained" type="submit">Submit</Button>
       </form>
     </Box>
