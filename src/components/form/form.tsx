6d8450ea2e3908be733095a093fb7e7b04163e49
Recriando Games <gabriel.mattos17@hotmail.com>
2022-11-03 09:43:50 -0300

    React hoot form

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,29 +1,33 @@
 import { Box } from "@mui/material";
 import Button from "@mui/material/Button";
-import { red } from "@mui/material/colors";
 import TextField from "@mui/material/TextField";
+import { useForm } from "react-hook-form";
 import "./form.css";
 
 const Form = () => {
+
+    const {register, handleSubmit, formState: {errors} } = useForm()
+
+    console.log(errors)
+
   return (
-        <Box className="box"
-        sx={{
-            width: 500,
-            height: 500,
-            backgroundColor: "white",
-        }}
-        >
-        <div>
-            <h1 className="title">Register form</h1>
-        </div>
-        <div className="name">
-            <TextField id="" label="First Name" variant="filled" />
-            <TextField id="" label="Last Name" variant="filled" />
-        </div>
-        <div className="button">
-            <Button variant="contained">Submit</Button>
-        </div>
-        </Box>
+    <Box
+      className="box"
+      sx={{
+        width: 500,
+        height: 500,
+        backgroundColor: "white",
+      }}
+    >
+      <div>
+        <h1 className="title">Register form</h1>
+      </div>
+      <form onSubmit={handleSubmit((data) => {console.log(data)})}>
+        <TextField {...register("firstName", {required: 'This is required.'})} label="First Name" variant="filled" />
+        <TextField {...register("lastName", {required: 'This is required.', minLength: {value: 4, message: "Minimum length is 4"}})} label="Last Name" variant="filled" />
+        <Button variant="contained" type="submit">Submit</Button>
+      </form>
+    </Box>
   );
 };
 
