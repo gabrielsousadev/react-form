b77537e3a5391b7028459bf64604eeb08930aea5
Recriando Games <gabriel.mattos17@hotmail.com>
2022-11-03 09:14:11 -0300

    Name field

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,18 +1,23 @@
 import { Box } from "@mui/material";
 import { red } from "@mui/material/colors";
+import TextField from "@mui/material/TextField";
 import "./form.css";
 
 const Form = () => {
   return (
-        <Box
+        <Box className="box"
         sx={{
             width: 500,
             height: 500,
-            backgroundColor: "primary.dark",
+            backgroundColor: "white",
         }}
         >
         <div>
             <h1 className="title">Register form</h1>
+        </div>
+        <div className="name">
+            <TextField id="" label="First Name" variant="filled" />
+            <TextField id="" label="Last Name" variant="filled" />
         </div>
         </Box>
   );
